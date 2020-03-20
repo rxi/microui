c50288ee5b380f63326a26ed587fc7e93cfc2cd0
Nicolas Sauzede <nsauzede@laposte.net>
2020-03-20 01:49:58 +0100

    Fix windows (Msys2/mingw64) demo build

--- demo/build.sh
+++ demo/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if test `uname -o` == "Msys"; then
+    GL="opengl32"
+else
+    GL="GL"
+fi
 gcc main.c renderer.c ../src/microui.c -I../src\
-    -Wall -std=c11 -pedantic -lSDL2 -lGL -lm -O3 -g
+    -Wall -std=c11 -pedantic `sdl2-config --libs` -l${GL} -lm -O3 -g
 
