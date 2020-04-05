55574143aa198432d412559bbba8ca4843eec57a
rxi <rxi@users.noreply.github.com>
2020-04-05 10:18:30 +0100

    Version 1.03
    Updated copyright year

--- src/microui.c
+++ src/microui.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2019 rxi
+** Copyright (c) 2020 rxi
 **
 ** Permission is hereby granted, free of charge, to any person obtaining a copy
 ** of this software and associated documentation files (the "Software"), to
--- src/microui.h
+++ src/microui.h
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2019 rxi
+** Copyright (c) 2020 rxi
 **
 ** This library is free software; you can redistribute it and/or modify it
 ** under the terms of the MIT license. See `microui.c` for details.
@@ -10,7 +10,7 @@
 
 #include <stdlib.h>
 
-#define MU_VERSION "1.02"
+#define MU_VERSION "1.03"
 
 #define MU_COMMANDLIST_SIZE     (1024 * 256)
 #define MU_ROOTLIST_SIZE        32
