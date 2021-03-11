bc98ad54a617e7348ac944c3200f9f591c439669
Kelvin M. Klann <kmk3.code@protonmail.com>
2021-03-11 01:55:28 -0300

    build.sh: simplify os check
    By turning the tests into a case statement.
    
    Note: This also removes "==", which is non-POSIX.

--- demo/build.sh
+++ demo/build.sh
@@ -2,13 +2,14 @@
 
 OS_NAME=`uname -o 2>/dev/null || uname -s`
 
-if [ $OS_NAME == "Msys" ]; then
-    GLFLAG="-lopengl32"
-elif [ $OS_NAME == "Darwin" ]; then
-    GLFLAG="-framework OpenGL"
-else
-    GLFLAG="-lGL"
-fi
+case "$OS_NAME" in
+Msys)
+    GLFLAG="-lopengl32";;
+Darwin)
+    GLFLAG="-framework OpenGL";;
+*)
+    GLFLAG="-lGL";;
+esac
 
 CFLAGS="-I../src -Wall -std=c11 -pedantic `sdl2-config --libs` $GLFLAG -lm -O3 -g"
 
