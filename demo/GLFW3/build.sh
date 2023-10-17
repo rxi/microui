50792fb659acac2bd4ceefb1939eab8c6846e8e1
Akmubi Lin <akmubi@yandex.ru>
2023-10-18 00:15:47 +0500

    add GLFW implementation

---
+++ demo/GLFW3/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OS_NAME=`uname -o 2>/dev/null || uname -s`
+
+if [ $OS_NAME == "Msys" ]; then
+    GLFLAG="-lopengl32"
+elif [ $OS_NAME == "Darwin" ]; then
+    GLFLAG="-framework OpenGL"
+else
+    GLFLAG="-lGL"
+fi
+
+CFLAGS="-I../../src -Wall -std=c11 -pedantic `pkg-config --libs glfw3` $GLFLAG -lm -O3 -g"
+
+gcc main.c renderer.c ../../src/microui.c $CFLAGS
+
